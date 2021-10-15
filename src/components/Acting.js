cbee61ec10d3ce85c1f97debfdacc6267f074491
Max <mverost4422@gmail.com>
2021-10-15 13:28:21 -0400

    Styled youtube iframe to be responsive and always full width w/ proportionate scaling.

--- src/components/Acting.js
+++ src/components/Acting.js
@@ -4,15 +4,15 @@ export function Acting({ selectedVideo }) {
   return (
     <>
       <h6>{selectedVideo.title}</h6>
-      <div className="about-video">
+      <div className="acting-video">
         <iframe
           width="560"
-          height="349"
+          height="315"
           src={`https://www.youtube-nocookie.com/embed/${selectedVideo.id}?autoplay=1&modestbranding=1&controls=1`}
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
     </>
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export function VideoTitle({ id, title, setVideo, selectedVideo }) {
+  const [selected, setSelected] = useState(false);
+
+  useEffect(() => {
+    if (selectedVideo.title === title) {
+      setSelected(true);
+    } else {
+      setSelected(false);
+    }
+  }, [selectedVideo, title]);
+
   return (
     <>
-      <h3
-        className={selectedVideo.title === title ? "selected-video" : ""}
-        onClick={() => setVideo({ title: title, id: id })}
-      >
-        {title}
-      </h3>
+      <h3 onClick={() => setVideo({ title: title, id: id })}>{title}</h3>
+      {selected && <div className="link-underline"></div>}
     </>
   );
 }
